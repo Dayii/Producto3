e9a69255db60511a8b8404578a7cc5130e6bf44c
dayii <dianateranargandona@gmail.com>
2019-03-23 11:27:35 -0400

    API mas azure

---
+++ productoBackend/api2/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace api2.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<api2.Models.Product> Products { get; set; }
+    }
+}
---
+++ productoBackend/api2/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api2.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del Producto")]
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+    }
+}
